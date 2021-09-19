ba90cff47681062109b672900dacd0987cbf8840
evaldez <edgarvaldez99@gmail.com>
2021-09-19 07:13:31 -0400

    feat: added backend test services

---
+++ src/app/services/backend-test.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+type Alive = { alive: boolean };
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendTestService {
+
+  private url = `${environment.api}/alive/`;
+
+  constructor(private http: HttpClient) { }
+
+  alive(): Observable<Alive> {
+    return this.http.get<Alive>(this.url);
+  }
+}
