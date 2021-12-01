41c22d7dabdf6ab1b4f3f021fe378f3f6f3f1bbf
evaldez <edgarvaldez99@gmail.com>
2021-11-30 22:35:09 -0300

    refactor:  update camion interface

--- src/app/services/camion.service.ts
+++ src/app/services/camion.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { CamionList, mockCamionList } from 'src/app/interfaces/camion';
+import { CamionList } from 'src/app/interfaces/camion';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,6 @@ export class CamionService {
   constructor(private http: HttpClient) { }
 
   getListByPropietarioId(propietarioId: number): Observable<CamionList[]> {
-    return of(mockCamionList);
+    return this.http.get<CamionList[]>(`${this.url}/propietario/${propietarioId}/`);
   }
 }
