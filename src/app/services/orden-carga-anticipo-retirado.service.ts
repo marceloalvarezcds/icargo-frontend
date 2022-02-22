7e1a1c3c3d727a236cc7c7de7b2ce758a9b6e1ea
evaldez <edgarvaldez99@gmail.com>
2022-02-22 16:14:23 -0300

    feat: added pdf button

--- src/app/services/orden-carga-anticipo-retirado.service.ts
+++ src/app/services/orden-carga-anticipo-retirado.service.ts
@@ -5,23 +5,29 @@ import { environment } from 'src/environments/environment';
 import { OrdenCargaAnticipoRetirado } from '../interfaces/orden-carga-anticipo-retirado';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrdenCargaAnticipoRetiradoService {
-
   private url = `${environment.api}/orden_carga_anticipo_retirado`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   create(formData: FormData): Observable<OrdenCargaAnticipoRetirado> {
     return this.http.post<OrdenCargaAnticipoRetirado>(`${this.url}/`, formData);
   }
 
   edit(id: number, formData: FormData): Observable<OrdenCargaAnticipoRetirado> {
-    return this.http.put<OrdenCargaAnticipoRetirado>(`${this.url}/${id}`, formData);
+    return this.http.put<OrdenCargaAnticipoRetirado>(
+      `${this.url}/${id}`,
+      formData
+    );
   }
 
   delete(id: number): Observable<OrdenCargaAnticipoRetirado> {
     return this.http.delete<OrdenCargaAnticipoRetirado>(`${this.url}/${id}`);
+  }
+
+  pdf(id: number): Observable<string> {
+    return this.http.get<string>(`${this.url}/${id}/pdf`);
   }
 }
