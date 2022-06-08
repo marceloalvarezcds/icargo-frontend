3eaf82e2fa94755b5f100fe73b536dbd411c4c32
evaldez <edgarvaldez99@gmail.com>
2022-06-08 12:54:33 -0400

    refactor: layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -6,15 +6,9 @@ import {
   ViewChild,
 } from '@angular/core';
 import { MatDrawerMode, MatSidenav } from '@angular/material/sidenav';
-import { filter } from 'rxjs/operators';
-import {
-  PermisoAccionEnum as a,
-  PermisoModeloEnum as m,
-} from 'src/app/enums/permiso-enum';
+import { Observable } from 'rxjs';
 import { MenuItem } from 'src/app/interfaces/menu-item';
-import { MenuConfigService } from 'src/app/services/menu-config.service';
-import { ResponsiveService } from 'src/app/services/responsive.service';
-import { UserService } from 'src/app/services/user.service';
+import { MenuService } from './menu.service';
 
 @Component({
   selector: 'app-layout',
@@ -22,199 +16,34 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnDestroy, AfterViewInit {
-  sidebarMode: MatDrawerMode = 'side';
-  menuList: MenuItem[] = [];
-  userSubscription = this.userService.getLoggedUser().subscribe(() => {
-    this.loadMenus();
-  });
-
-  toggleSidebarMenuSubscription = this.menuConfigService
-    .getToggleSidebarMenuObservable()
-    .pipe(filter(() => !!this.sidenav))
-    .subscribe((isOpened: boolean) => {
-      if (isOpened) {
-        this.sidenav!.open();
-      } else {
-        this.sidenav!.close();
-      }
-    });
-
-  @ViewChild('sidenav') sidenav?: MatSidenav;
-
   @HostListener('window:resize')
   onResize(): void {
-    this.configSidebarMode();
+    this.menuService.configSidebarModeByScreen();
   }
 
-  constructor(
-    public menuConfigService: MenuConfigService,
-    private responsiveService: ResponsiveService,
-    private userService: UserService
-  ) {}
+  @ViewChild('sidenav') set sidenav(nav: MatSidenav) {
+    this.menuService.setSidenav(nav);
+  }
+
+  get menuList$(): Observable<MenuItem[]> {
+    return this.menuService.list$;
+  }
+
+  get sidebarMode(): MatDrawerMode {
+    return this.menuService.sidebarMode;
+  }
+
+  constructor(private menuService: MenuService) {}
 
   ngOnDestroy(): void {
-    this.toggleSidebarMenuSubscription.unsubscribe();
-    this.userSubscription.unsubscribe();
+    this.menuService.unsubscribe();
   }
 
   ngAfterViewInit(): void {
-    this.configSidebarMode();
+    this.menuService.configSidebarModeByScreen();
   }
 
   closeSidebarMenu(): void {
-    if (this.responsiveService.isMobileScreen) {
-      this.menuConfigService.setSidebarMenu(false);
-    }
-  }
-
-  private configSidebarMode(): void {
-    setTimeout(() => {
-      if (this.responsiveService.isMobileScreen) {
-        this.sidebarMode = 'over';
-        this.menuConfigService.setSidebarMenu(false);
-      } else {
-        this.sidebarMode = 'side';
-        this.menuConfigService.setSidebarMenu(true);
-      }
-    });
-  }
-
-  private loadMenus(): void {
-    this.menuList = [
-      {
-        name: 'Inicio',
-        iconName: 'speed',
-        path: '',
-        active: true,
-        isRouteExact: true,
-      },
-      {
-        name: 'Entidades',
-        iconName: 'business',
-        active: true,
-        children: [
-          {
-            name: 'Centros Operativos',
-            iconName: 'public',
-            path: `/entities/${m.CENTRO_OPERATIVO}`,
-            active: this.userService.checkPermiso(a.LISTAR, m.CENTRO_OPERATIVO),
-          },
-          {
-            name: 'Remitentes',
-            iconName: 'open_with',
-            path: `/entities/${m.REMITENTE}`,
-            active: this.userService.checkPermiso(a.LISTAR, m.REMITENTE),
-          },
-          {
-            name: 'Proveedores',
-            iconName: 'event_available',
-            path: `/entities/${m.PROVEEDOR}`,
-            active: this.userService.checkPermiso(a.LISTAR, m.PROVEEDOR),
-          },
-          {
-            name: 'Gestores de Carga',
-            iconName: 'directions_bus',
-            iconOutline: true,
-            path: `/entities/${m.GESTOR_CARGA}`,
-            active: this.userService.checkPermiso(a.LISTAR, m.GESTOR_CARGA),
-          },
-        ],
-      },
-      {
-        name: 'Flota',
-        iconName: 'local_shipping',
-        iconOutline: true,
-        active: true,
-        children: [
-          {
-            name: 'Propietarios',
-            iconName: 'key',
-            path: `/flota/${m.PROPIETARIO}`,
-            active: this.userService.checkPermiso(a.LISTAR, m.PROPIETARIO),
-          },
-          {
-            name: 'Choferes',
-            iconName: 'badge',
-            iconOutline: true,
-            path: `/flota/${m.CHOFER}`,
-            active: this.userService.checkPermiso(a.LISTAR, m.CHOFER),
-          },
-          {
-            name: 'Camiones',
-            iconName: 'local_shipping',
-            iconOutline: true,
-            path: `/flota/${m.CAMION}`,
-            active: this.userService.checkPermiso(a.LISTAR, m.CAMION),
-          },
-          {
-            name: 'Semi-remolques',
-            iconName: 'local_shipping',
-            path: `/flota/${m.SEMIRREMOLQUE}`,
-            active: this.userService.checkPermiso(a.LISTAR, m.SEMIRREMOLQUE),
-          },
-        ],
-      },
-      {
-        name: 'Flete',
-        iconName: 'timeline',
-        iconOutline: true,
-        path: `/flete/${m.FLETE}`,
-        active: this.userService.checkPermiso(a.LISTAR, m.FLETE),
-      },
-      {
-        name: 'Orden de Carga',
-        iconName: 'description',
-        iconOutline: true,
-        path: `/orden-carga/${m.ORDEN_CARGA}`,
-        active: this.userService.checkPermiso(a.LISTAR, m.ORDEN_CARGA),
-      },
-      {
-        name: 'Caja & Banco',
-        iconName: 'account_balance_wallet',
-        // iconName: 'local_atm',
-        iconOutline: true,
-        active: true,
-        children: [
-          {
-            name: 'Caja',
-            iconName: 'inventory_2',
-            iconOutline: true,
-            path: `/caja/${m.CAJA}`,
-            active: this.userService.checkPermiso(a.LISTAR, m.CAJA),
-          },
-          {
-            name: 'Banco',
-            iconName: 'account_balance',
-            iconOutline: true,
-            path: `/banco/${m.BANCO}`,
-            active: this.userService.checkPermiso(a.LISTAR, m.BANCO),
-          },
-        ],
-      },
-      {
-        name: 'Estado de Cuenta',
-        iconName: 'summarize',
-        iconOutline: true,
-        path: `/estado-cuenta/${m.ESTADO_CUENTA}`,
-        active: this.userService.checkPermiso(a.LISTAR, m.MOVIMIENTO),
-      },
-      {
-        name: 'Listados',
-        iconName: 'format_list_numbered',
-        // iconName: 'local_atm',
-        iconOutline: true,
-        active: true,
-        children: [
-          {
-            name: 'Rentabilidad',
-            iconName: 'request_quote',
-            iconOutline: true,
-            path: `/listado/${m.RENTABILIDAD}`,
-            active: this.userService.checkPermiso(a.LISTAR, m.RENTABILIDAD),
-          },
-        ],
-      },
-    ];
-    this.menuConfigService.setMenuItemList(this.menuList);
+    this.menuService.closeSidebarMenu();
   }
 }
