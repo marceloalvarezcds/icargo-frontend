705478fd568181e1fb2a93ecc6b08de160a8fe0b
evaldez <edgarvaldez99@gmail.com>
2022-07-27 18:54:32 -0400

    refactor: added flete result in rentabilidad

--- src/app/dialogs/movimiento-edit-by-merma-form-dialog/movimiento-edit-by-merma-form-dialog.component.spec.ts
+++ src/app/dialogs/movimiento-edit-by-merma-form-dialog/movimiento-edit-by-merma-form-dialog.component.spec.ts
@@ -1,13 +1,37 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { mockMovimientoMermaEditFormDialogData } from 'src/app/interfaces/movimiento-merma-edit-form-dialog-data';
+import { MaterialModule } from 'src/app/material/material.module';
+import { DialogsModule } from '../dialogs.module';
 
 import { MovimientoEditByMermaFormDialogComponent } from './movimiento-edit-by-merma-form-dialog.component';
 
 describe('MovimientoEditByMermaFormDialogComponent', () => {
   let component: MovimientoEditByMermaFormDialogComponent;
   let fixture: ComponentFixture<MovimientoEditByMermaFormDialogComponent>;
+  const dialogData = mockMovimientoMermaEditFormDialogData;
+  const mockDialogRefSpyObj = jasmine.createSpyObj({ close: () => {} });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        MaterialModule,
+        DialogsModule,
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: mockDialogRefSpyObj },
+        { provide: MAT_DIALOG_DATA, useValue: dialogData },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [ MovimientoEditByMermaFormDialogComponent ]
     })
     .compileComponents();
