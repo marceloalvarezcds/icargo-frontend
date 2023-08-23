c5bc5ab3b9b9b2d80ec38168b9fc24dec1d4c7fb
Edgar Valdez <edgarvaldez99@gmail.com>
2023-08-23 16:23:54 -0400

    refactor: sentryUrl

--- src/environments/environment.testing.ts
+++ src/environments/environment.testing.ts
@@ -3,5 +3,5 @@ export const environment = {
   api: 'https://testing.icargo.com.py/api',
   baseUrl: 'https://testing.icargo.com.py',
   sentryUrl:
-    'https://780015f8634f4511922da19312bb6299@o4504123932082176.ingest.sentry.io/4504214937075712',
+    'https://e8ca62a0bd14bff4979c6abd738ec6a8@o4504123932082176.ingest.sentry.io/4504214937075712',
 };
