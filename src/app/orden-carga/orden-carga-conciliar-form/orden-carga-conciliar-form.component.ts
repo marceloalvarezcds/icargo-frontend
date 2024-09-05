11dbccae2b30e79fb198f5dac714941b39cfc02b
DARIOORTIZ <ortiz.dario94@gmail.com>
2024-09-05 11:46:51 -0400

    Finalizar fix botones

---
+++ src/app/orden-carga/orden-carga-conciliar-form/orden-carga-conciliar-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orden-carga-conciliar-form',
+  templateUrl: './orden-carga-conciliar-form.component.html',
+  styleUrls: ['./orden-carga-conciliar-form.component.scss']
+})
+export class OrdenCargaConciliarFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
