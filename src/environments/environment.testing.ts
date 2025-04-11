c997e20fd0523df02307c2509e3334a9c2d13e2d
sdakef <dantepane@gmail.com>
2025-04-11 15:22:10 -0300

    update urls

--- src/environments/environment.testing.ts
+++ src/environments/environment.testing.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api: 'https://testing.icargo.com.py/api',
-  baseUrl: 'https://testing.icargo.com.py',
+  api: 'https://sistema.icargo.com.py/api',
+  baseUrl: 'https://sistema.icargo.com.py',
   // sentryUrl:
   //   'https://e8ca62a0bd14bff4979c6abd738ec6a8@o4504123932082176.ingest.sentry.io/4504214937075712',
   sentryUrl: '',
